679ea39089f28b50b9eced505d044243672b0ce7
Лаптырев Алексей <leshka.l1337@MacBook-Pro-Laptyrev.local>
2019-07-15 13:38:13 +0300

    addFunction addPost

--- src/redux/state.js
+++ src/redux/state.js
@@ -28,8 +28,14 @@ let state = {
     }
 }
 
-let addPost = () => {
-    
+export let addPost = (postMessage) => {
+    let newPost = {
+        id: 5,
+        message: postMessage,
+        likesCount: 0
+    }
+
+    state.profilePage.posts.push(newPost)
 }
 
 export default state
