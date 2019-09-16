e9c45e7936652762961b368c6c2ed36b841eaef1
SOUTHYYY <leshka.l1337@gmail.com>
2019-09-16 13:06:23 +0300

    added auth method

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -3,12 +3,14 @@ import profileReduser from './profile-reduser'
 import dialogsReduser from './dialogs-reduser'
 import navbarReduser from './navbar-reduser'
 import findFriendsReduser from "./find-friends-reduser";
+import authReduser from "./auth-reduser";
 
 let reducers = combineReducers({
     profilePage: profileReduser,
     messagesPage: dialogsReduser,
     navbar: navbarReduser,
     findFriends: findFriendsReduser,
+    auth: authReduser
 })
 
 let store = createStore(reducers)
