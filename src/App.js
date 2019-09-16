787399ed2008620e734c595b8b9d5622473d2ec1
SOUTHYYY <leshka.l1337@gmail.com>
2019-09-16 13:02:53 +0300

    add HeaderContainer component

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React from 'react';
 import {Route} from 'react-router-dom' // пакет для роутинга 
 import './App.css';
-import Header from './components/Header/Header';
+import HeaderContainer from './components/Header/HeaderContainer';
 import Navbar from './components/Navbar/Navbar';
 import ProfileContainer from './components/Profile/ProfileContainer';
 import DialogsContainer from './components/Dialogs/DialogsContainer';
 import FindFriendsContainer from './components/FindFriends/FindFriendsContainer'
+import ScrollToTop from './components/Common/ScrollToTop/scrollToTop'
 
 const App = (props) => {
 
   return (
       <div className='app-wrapper'>
-        <Header />
-        <Navbar />
-        <div className='app-wrapper-content'>
-          <Route path='/dialogs' render={ () => <DialogsContainer />} />
-          <Route path='/profile/:userId?' render={ () => <ProfileContainer />}/>
-          <Route path='/find-friends' render={ () => <FindFriendsContainer />} />
-          <Route />
-          <Route />
-          <Route />
-        </div>
+          <HeaderContainer />
+          <Navbar />
+          <div className='app-wrapper-content'>
+            <ScrollToTop >
+              <Route path='/dialogs' render={ () => <DialogsContainer />} />
+              <Route path='/profile/:userId?' render={ () => <ProfileContainer />}/>
+              <Route path='/find-friends' render={ () => <FindFriendsContainer />} />
+              <Route />
+              <Route />
+              <Route />
+            </ScrollToTop>
+          </div>
       </div>
   )
 }
