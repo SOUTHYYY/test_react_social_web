cf192092362c329663d1816c8fe2f52b187ebce2
SOUTHYYY <leshka.l1337@gmail.com>
2019-09-23 18:32:43 +0300

    add redux-thunk

--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -1,9 +1,10 @@
-import { createStore, combineReducers } from "redux";
+import { createStore, combineReducers, applyMiddleware } from "redux";
 import profileReduser from './profile-reduser'
 import dialogsReduser from './dialogs-reduser'
 import navbarReduser from './navbar-reduser'
 import findFriendsReduser from "./find-friends-reduser";
 import authReduser from "./auth-reduser";
+import thunkMiddleeware from 'redux-thunk'
 
 let reducers = combineReducers({
     profilePage: profileReduser,
@@ -13,7 +14,7 @@ let reducers = combineReducers({
     auth: authReduser
 })
 
-let store = createStore(reducers)
+let store = createStore(reducers, applyMiddleware(thunkMiddleeware))
 
 window.store = store
 
