72932221a71c8bbdbe9a1a783d7f876e23076e72
SOUTHYYY <leshka.l1337@gmail.com>
2019-10-09 11:02:56 +0300

    add current date rendering

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -7,17 +7,30 @@ import Post from './Post/Post'
 const MyPosts = (props) => {
 
     let postsElements = 
-        props.posts.map( post => <Post message={post.message} likesCount={post.likesCount} postAuthor={props.postAuthor} key={post.id} />)
+        props.posts.map( post => 
+        <Post message={post.message} 
+        likesCount={post.likesCount}
+        postAuthor={props.postAuthor}
+        date={post.date} 
+        key={post.id} />)
 
     let newPostElement = React.createRef()
     let onAddPost = () => {
         let text = newPostElement.current.value
         if(text === '' || text === ' ') return
-        else props.addPost() 
+        else{
+            props.addPost()
+        }
     }
     let onPostChange = () => {
         let text = newPostElement.current.value
-        props.updateNewPostText(text)   
+        props.updateNewPostText(text) 
+    }
+    let handleKeyPress = (target) => {
+        if(target.charCode===13){
+            console.log('123')
+            onAddPost()
+        }
     }
     return(
         <div className={styles.postsBlock}>
@@ -25,12 +38,13 @@ const MyPosts = (props) => {
                 <h3>Мои записи</h3>
                 <div className={styles.news_top_block}>
                     <div className={styles.wrapper_textArea}>
-                        <textarea className={styles.textArea} 
-                            ref={newPostElement} 
-                            onChange={onPostChange}
-                            placeholder='Что у вас нового?' 
-                            value={props.newPostText}
-                        />
+                        <input className={styles.textArea}
+                        placeholder='Что у вас нового?'
+                        onKeyPress={handleKeyPress}
+                        value={props.newPostText}
+                        onChange={onPostChange}
+                        type="text" autoFocus
+                        ref={newPostElement}/>
                     </div>
                     <div>
                         <button onClick={onAddPost}>Add Post</button>
--- src/components/Profile/MyPosts/Post/Post.jsx
+++ src/components/Profile/MyPosts/Post/Post.jsx
@@ -4,9 +4,12 @@ import styles from './Post.module.css'
 const Post = (props) => {
     return(
         <div className={styles.item_wrapper}>
-            <div className={styles.item_info}>
+            <div className={styles.item_info_container}>
                 <img src='https://pp.userapi.com/c855024/v855024603/ac9e4/cZnQYXZ7a8E.jpg?ava=1' alt=""></img>
-                {props.postAuthor}
+                <div className={styles.item_Info}>
+                    <p>{props.postAuthor}</p>
+                    <p>{props.date}</p>
+                </div>
             </div>
             <div className={styles.post}>
                 {props.message}
