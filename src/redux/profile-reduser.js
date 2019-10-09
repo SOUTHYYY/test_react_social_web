57aa69204fd710c26b0edadfad0da0d6ef84d7e5
SOUTHYYY <leshka.l1337@gmail.com>
2019-10-09 11:02:05 +0300

    added post date creation in dd-mm hh-mm format

--- src/redux/profile-reduser.js
+++ src/redux/profile-reduser.js
@@ -6,9 +6,9 @@ const SET_USER_PROFILE = 'SET_USER_PROFILE'
 
 let initialState = {
     posts: [
-        { id: 3, message: "Я в лазурном", likesCount: 1 },
-        { id: 2, message: "It's my first post", likesCount: 15 },
-        { id: 1, message: "Hello everybody", likesCount: 3 },
+        { id: 3, message: "Я в лазурном", date: '24 авг в 12:30', likesCount: 1},
+        { id: 2, message: "It's my first post", date: '30 июл в 19:01', likesCount: 15 },
+        { id: 1, message: "Hello everybody", date: '30 июл в 19:00', likesCount: 3 },
     ],
     newPostText: '',
     postAuthor: 'Лаптырев Алексей',
@@ -18,11 +18,35 @@ let initialState = {
 const profileReduser = (state = initialState, action) => {
     switch (action.type) {
         case ADD_POST: {
+            const months = {
+                0: 'янв',
+                1: 'фев',
+                2: 'мар',
+                3: 'апр',
+                4: 'мая',
+                5: 'июн',
+                6: 'июл',
+                7: 'авг',
+                8: 'сен',
+                9: 'окт',
+                10:'ноя',
+                11:'дек'
+              }
+            let date = new Date()
+            const days = date.getDate()
+            const mouthIndex = date.getMonth()
+            const monthName = months[mouthIndex]
+            const hours = date.getHours()
+            const minutes = date.getMinutes()
+            let currentDate =  `${days} ${monthName} в ${hours}:${minutes}`
+            debugger
             let newPost = {
                 id: state.posts.lenght + 1,
+                date: currentDate,
                 message: state.newPostText,
                 likeCount: 0,
             }
+            debugger
             return {
                 ...state,
                 posts: [newPost, ...state.posts],
@@ -46,8 +70,8 @@ const profileReduser = (state = initialState, action) => {
     }
 }
 
-export const addPostActionCreator = () => ({ type: ADD_POST })
-export const updateNewPostTextActionCreator = (text) =>
+export const addPost = () => ({ type: ADD_POST})
+export const updateNewPostText = (text) =>
     ({ type: UPDATE_NEW_POST_TEXT, newText: text })
 export const setUserProfileSuccess = (profile) => ({type: SET_USER_PROFILE, profile})
 
