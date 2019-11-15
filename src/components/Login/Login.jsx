538a2f0e763a75ee5ea7b0a89a0824bacf3a45f7
SOUTHYYY <leshka.l1337@gmail.com>
2019-11-15 09:23:38 +0300

    use createField() in LooginForm

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,27 +1,59 @@
 import React from 'react'
 import styles from './Login.module.css'
+import { reduxForm, Field} from 'redux-form'
+import { Input} from '../Common/FormsControl/FormsControl'
+import {createField} from '../Common/FormsControl/FormsControl'
+import { required, maxLengthCreator } from '../Utils/Validators/validator'
+import { connect } from 'react-redux'
+import { login } from '../../redux/auth-reduser'
+import { Redirect } from 'react-router-dom'
+import error from '../Common/FormsControl/FormsControl.module.css'
+
+const maxLength30 = maxLengthCreator(30)
+
+const LoginForm = ({ handleSubmit, onSubmit, error }) => {
+    return (
+        <form onSubmit={handleSubmit} >
+
+            {createField('Email', 'email', [required], Input)}
+            {createField('Password', 'password', [required], Input)}
+            <div>
+                <Field component={'input'} name={'rememberMe'} type={"checkbox"} />Запомнить меня
+            </div>
+            {error &&
+                <div className={error.formSummaryError}>
+                    {error}
+                </div>
+            }
+            <div className={styles.acceptRegistrationСontainer}>
+                <button className={styles.acceptRegistrationButton}
+                    onClick={onSubmit}>Войти</button>
+            </div>
+        </form>
+    )
+}
+
+const LoginReduxForm = reduxForm({ form: 'login' })(LoginForm)
 
 const Login = (props) => {
+    const onSubmit = (formData) => {
+        props.login(formData.email, formData.password, formData.rememberMe)
+    }
+
+    if (props.isAuth) {
+        return <Redirect to={'/profile'} />
+    }
+
     return (
         <div className={styles.container}>
-            <div className={styles.formContainer}>
-                <form action="" method='post'>
-                    <h1>Еще не зарегестрированны?</h1>
-                    <div className={styles.string}>
-                        <label htmlFor='email' placeholder>Введите email</label>
-                        <input type="text" name='email'/>
-                    </div>
-                    <div className={styles.string}>
-                        <label htmlFor="password">Придумайте пароль</label>
-                        <input type="text" name='password'/>
-                    </div>
-                    <div className={styles.acceptRegistrationСontainer}>
-                        <button className={styles.acceptRegistrationButton}>Зарегистрироватсья</button>
-                    </div>
-                </form>
-            </div>
+            <h1>Еще не зарегестрированны?</h1>
+            <LoginReduxForm onSubmit={onSubmit} />
         </div>
     )
 }
 
-export default Login
+const mapStateToProps = (state) => ({
+    isAuth: state.auth.isAuth
+})
+
+export default connect(mapStateToProps, { login })(Login)
